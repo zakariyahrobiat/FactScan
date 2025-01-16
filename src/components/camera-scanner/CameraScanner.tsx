f1202a23feb0098a66f8d180d3b1adb889fe147b
zakariyahrobiat <zakariyahrobiat2018@gmail.com>
2025-01-16 17:14:53 +0100

    add z-index to the start scanning button

--- src/components/camera-scanner/CameraScanner.tsx
+++ src/components/camera-scanner/CameraScanner.tsx
@@ -10,7 +10,7 @@ const CameraScanner = () => {
     const {inputBarcode}= data
   return (
     
-    <div className="w-full px-5 md:px-20 bg-slate-100 py-4 gap-10 grid gri md:grid-cols-2 items-start h-full">
+    <div className="w-full px-5 md:px-20 bg-slate-100 py-4 gap-10 grid md:grid-cols-2 items-start h-full">
     <div className="scanner-overlay  ">
     <div className="overlay"></div>
   <div className="overlay-frame  "></div>
@@ -21,14 +21,14 @@ const CameraScanner = () => {
       {barCode && <p className="mt-4 text-green-600 font-medium">Detected Barcode: <span className="font-bold">{barCode}</span></p>}
       {error && <p className="mt-4 text-red-500 font-medium">Error: <span className="font-bold">{error}</span></p>}
       <button
-          className=" absolute bottom-3 bg-blue-500 text-white px-5 py-2 rounded shadow-md"
+          className=" absolute bottom-3 z-10 bg-blue-500 text-white px-5 py-2 rounded shadow-md"
           onClick={startScanner}
         >
           Start Scanning
         </button>
     </div>
 
-    <div className="p-6 bg-white rounded shadow-md flex flex-col items-center">
+    <div className="p-6 h-full bg-white rounded shadow-md flex flex-col items-center">
  
     <p className="text-gray-600 text-lg font-medium mb-4">
           Align the barcode within the frame or enter it manually
