9d5a0d7e9f7f479da3accb8a88efde9e71de31cd
zakariyahrobiat <zakariyahrobiat2018@gmail.com>
2025-01-29 16:19:37 +0100

    styled the register page

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,12 +1,20 @@
 import CustomInput from "../../components/inputs/CustomInput"
 import BackgroundLayout from "../../Layout/BackgroundLayout"
+import image from "../../assets/images/barcode-scanner.jpg"
 const Register = () => {
   return (
     <BackgroundLayout>
+      <div className="grid grid-cols-1 gap-x-20 md:grid-cols-2 bg-white shadow-lg m-5 p-10 h-screen overflow-hidden">
+        <div className=" h-full flex justify-center items-center  ">
+          <img src={image} alt="" srcSet="" className="w-full h-full object-cover rounded-r-2xl" />
+        </div>
+      <form action="" className="mr-20 my-10">
       <CustomInput label="name" name="name" id="name"/>
         <CustomInput  label="Email" name="email" id="email" type="email"/>
         <CustomInput label="password" name="password" id="password" type="password"/>
         <button>Sign Up</button>
+        </form>
+        </div>
     </BackgroundLayout>
   )
 }
